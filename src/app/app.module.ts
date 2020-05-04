99f99b0d316fc21f806799789600ea9603433e68
gretanorv <gretanorv@gmail.com>
2020-05-04 15:30:54 +0300

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardCardComponent } from './dashboard-card/dashboard-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardCardComponent
   ],
   imports: [
     BrowserModule,
