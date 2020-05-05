ba65c9409e21452e6b146205faec38d3ad0d09a5
gretanorv <gretanorv@gmail.com>
2020-05-05 16:45:27 +0300

    dashboard card big & icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardCardComponent } from './dashboard-card/dashboard-card.component';
 import { DashboardCardBigComponent } from './dashboard-card-big/dashboard-card-big.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardCardComponent,
     DashboardCardBigComponent
   ],
   imports: [
--- src/app/dashboard-card-big/dashboard-card-big.component.ts
+++ src/app/dashboard-card-big/dashboard-card-big.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-card-big',
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard-card-big.component.scss']
 })
 export class DashboardCardBigComponent implements OnInit {
+
+  @Input()
+  colorClass: string;
+  @Input()
+  username: string;
+  @Input()
+  iconLink: string;
+  @Input()
+  followersNumber: string;
+  @Input()
+  numberOfFolowers: string;
+  @Input()
+  progressIcon: string;
+  @Input()
+  statusIconLink: string;
 
   constructor() { }
 
