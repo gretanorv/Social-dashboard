7e12ba9a8c10f84aedd399e243d09330475a802a
gretanorv <gretanorv@gmail.com>
2020-05-06 17:43:07 +0300

    today progress done

---
+++ src/app/dashboard-today-progress/dashboard-today-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-today-progress',
+  templateUrl: './dashboard-today-progress.component.html',
+  styleUrls: ['./dashboard-today-progress.component.scss']
+})
+export class DashboardTodayProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
