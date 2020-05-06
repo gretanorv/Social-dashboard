1f43f678184501e0e46b471157dbf39d2e63fca6
gretanorv <gretanorv@gmail.com>
2020-05-06 13:21:04 +0300

    empty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DashboardCardBigComponent } from './dashboard-card-big/dashboard-card-big.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToggleSliderComponent } from './shared/toggle-slider/toggle-slider.component';
+import { DashboardCardSmallComponent } from './dashboard-card-small/dashboard-card-small.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardCardBigComponent,
     DashboardComponent,
-    ToggleSliderComponent
+    ToggleSliderComponent,
+    DashboardCardSmallComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard-card-small/dashboard-card-small.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-card-small',
+  templateUrl: './dashboard-card-small.component.html',
+  styleUrls: ['./dashboard-card-small.component.scss']
+})
+export class DashboardCardSmallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
