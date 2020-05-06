81dd27601e8827b88af80ea34c91dd4e603a371e
gretanorv <gretanorv@gmail.com>
2020-05-06 16:15:21 +0300

    small card component & class naming

--- src/app/dashboard-card-small/dashboard-card-small.component.ts
+++ src/app/dashboard-card-small/dashboard-card-small.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { } from '../dashboard-card-big/dashboard-card-big.component'
 
 @Component({
   selector: 'app-dashboard-card-small',
   templateUrl: './dashboard-card-small.component.html',
-  styleUrls: ['./dashboard-card-small.component.scss']
+  styleUrls: ['./dashboard-card-small.component.scss',
+    '../dashboard-card-big/dashboard-card-big.component.scss']
 })
 export class DashboardCardSmallComponent implements OnInit {
+
+  @Input()
+  cardTitle: string;
+  @Input()
+  iconLink: string;
+  @Input()
+  followersNumber: string;
+  @Input()
+  statusIconLink: string;
+  @Input()
+  statusColor: string;
+  @Input()
+  followersPercentage: string;
 
   constructor() { }
 
